1cddc355bbb208f53d2e1119241818cf609c0309
Albasuri <albasuriavillacampa@gmail.com>
2025-11-22 22:32:07 +0100

    Crear Cluser formulari

--- Alba-dev/frontend/src/App.js
+++ Alba-dev/frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import ProtectedRoute from './components/ProtectedRoute';
+import CreateCluster from './components/CreateCluster'; 
 import './App.css';
 
 function App() {
@@ -13,11 +14,21 @@ function App() {
         <div className="app">
           <Routes>
             <Route path="/login" element={<Login />} />
+            
+            {/* Rutes Protegides (Només accessibles amb Login) */}
             <Route path="/dashboard" element={
               <ProtectedRoute>
                 <Dashboard />
               </ProtectedRoute>
             } />
+            
+            {/* Movem la ruta aquí dins perquè sigui privada */}
+            <Route path="/create-cluster" element={
+              <ProtectedRoute>
+                <CreateCluster />
+              </ProtectedRoute>
+            } />
+
             <Route path="/" element={<Navigate to="/login" />} />
           </Routes>
         </div>
